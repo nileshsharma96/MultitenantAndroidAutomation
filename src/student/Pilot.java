00529be9f452799ede53ba082e2af1eaedd93297
nileshsharma96 <89655243+nileshsharma96@users.noreply.github.com>
2021-11-08 15:16:18 +0530

    Update Pilot.java

--- src/student/Pilot.java
+++ src/student/Pilot.java
@@ -1,6 +1,5 @@
-package student;
+package Student;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -14,17 +13,18 @@ public class Pilot {
 
 	public static AndroidDriver<AndroidElement> capabilities(String device) throws MalformedURLException {
 		// TODO Auto-generated method stub
-		File appDir = new File("src");
-		File app = new File(appDir, "Student-2021.002.08-" + EnvConfig.flavour + "-" + EnvConfig.varient + ".apk");
+//		File appDir = new File("src");
+//		File app = new File(appDir, "Student-2021.002.08-" + EnvConfig.flavour + "-" + EnvConfig.varient + ".apk");
 		DesiredCapabilities cap = new DesiredCapabilities();
 		if (device.equals("emulator")) {
 			cap.setCapability(MobileCapabilityType.DEVICE_NAME, "NileshDvc");
 		} else if (device.equals("real")) {
 			cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Device");
 		}
-		cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+//		cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
 
 		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+		cap.setCapability(MobileCapabilityType.APPLICATION_NAME, "LIL@Home");
 
 		AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
 
